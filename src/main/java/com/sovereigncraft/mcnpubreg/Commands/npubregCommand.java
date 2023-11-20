44d70cba0a7f586728bd18399f1ad9b844a0122f
Sovtoshi <sovtoshi@sovereigncraft.com>
2023-11-19 22:44:49 -0700

    Setup of command basic structure.

---
+++ src/main/java/com/sovereigncraft/mcnpubreg/Commands/npubregCommand.java
@@ -0,0 +1,14 @@
+package com.sovereigncraft.mcnpubreg.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class npubregCommand implements org.bukkit.command.CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+     return true;
+
+    }
+}
--- src/main/java/com/sovereigncraft/mcnpubreg/MC_npubreg.java
+++ src/main/java/com/sovereigncraft/mcnpubreg/MC_npubreg.java
@@ -1,13 +1,18 @@
 package com.sovereigncraft.mcnpubreg;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import com.sovereigncraft.mcnpubreg.Commands.*;
+import lombok.SneakyThrows;
 
 public final class MC_npubreg extends JavaPlugin {
-
+    private static MC_npubreg instance;
     @Override
+    @SneakyThrows
     public void onEnable() {
-        // Plugin startup logic
 
+        // Plugin startup logic
+        instance = this;
+        this.getCommand("npubreg").setExecutor(new npubregCommand());
     }
 
     @Override
