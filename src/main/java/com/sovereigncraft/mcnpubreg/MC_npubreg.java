4444620ee1434007bc6b9df8fcc35c48a5df8ec9
CappyTech <133281784+CappyTech@users.noreply.github.com>
2023-11-21 00:20:57 +0000

    Update MC_npubreg.java

--- src/main/java/com/sovereigncraft/mcnpubreg/MC_npubreg.java
+++ src/main/java/com/sovereigncraft/mcnpubreg/MC_npubreg.java
@@ -1,22 +1,29 @@
 package com.sovereigncraft.mcnpubreg;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import com.sovereigncraft.mcnpubreg.Commands.*;
-import lombok.SneakyThrows;
+import com.sovereigncraft.mcnpubreg.Commands.npubregCommand;
 
 public final class MC_npubreg extends JavaPlugin {
     private static MC_npubreg instance;
-    @Override
-    @SneakyThrows
-    public void onEnable() {
 
-        // Plugin startup logic
+    @Override
+    public void onEnable() {
         instance = this;
+
+        // Register commands
         this.getCommand("npubreg").setExecutor(new npubregCommand());
+
+        // Any other startup logic
+        getLogger().info("MC_npubreg has been enabled!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("MC_npubreg has been disabled.");
+    }
+
+    public static MC_npubreg getInstance() {
+        return instance;
     }
 }
