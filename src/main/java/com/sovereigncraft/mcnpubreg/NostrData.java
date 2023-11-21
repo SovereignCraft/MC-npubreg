0c6f1e09eb3c453e4510173cac4dcb182deda536
CappyTech <133281784+CappyTech@users.noreply.github.com>
2023-11-21 00:20:55 +0000

    Create NostrData.java

---
+++ src/main/java/com/sovereigncraft/mcnpubreg/NostrData.java
@@ -0,0 +1,19 @@
+package com.sovereigncraft.mcnpubreg;
+
+public class NostrData {
+    private String npub;
+
+    // Constructor, getters, and setters
+    public NostrData(String npub) {
+        this.npub = npub;
+    }
+
+    public String getNpub() {
+        return npub;
+    }
+
+    public void setNpub(String npub) {
+        this.npub = npub;
+    }
+}
+
